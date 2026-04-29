a3134ea55287ddaf99baadc64c7517601d7adee3
strewili <shouqaaa123@gmail.com>
2026-04-29 19:59:33 +0300

    Add function to count binary tree nodes with children

--- binary_trees.h
+++ binary_trees.h
@@ -32,5 +32,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
-
+size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
