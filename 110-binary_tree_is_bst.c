e3ad3c10d6eb3409068b017a477e4496dae9ee0d
strewili <shouqaaa123@gmail.com>
2026-05-01 23:35:20 +0300

    fix110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,11 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * check_bst - helper.
+ * check_bst - Checks if a binary tree is a valid BST
+ * @tree: Pointer to the root node of the tree
+ * @min: Minimum allowed value
+ * @max: Maximum allowed value
+ *
+ * Return: 1 if valid, otherwise 0
  */
 int check_bst(const binary_tree_t *tree, int min, int max)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (1);
 
 	if (tree->n <= min || tree->n >= max)
@@ -16,11 +21,14 @@ int check_bst(const binary_tree_t *tree, int min, int max)
 }
 
 /**
- * binary_tree_is_bst - checks if valid BST.
+ * binary_tree_is_bst - Checks if a binary tree is a valid BST
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: 1 if valid BST, otherwise 0
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	return (check_bst(tree, -2147483648, 2147483647));
