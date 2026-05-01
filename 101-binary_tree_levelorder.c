cfca32028fdd8603e7a33a81396f3d69b3ab6a7b
strewili <shouqaaa123@gmail.com>
2026-05-01 23:30:39 +0300

    fix 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,27 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * height - Measures height.
+ * tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Height of the tree
  */
-size_t height(const binary_tree_t *tree)
+size_t tree_height(const binary_tree_t *tree)
 {
-	size_t l, r;
+	size_t left_height, right_height;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	l = height(tree->left);
-	r = height(tree->right);
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
 
-	return ((l > r ? l : r) + 1);
+	if (left_height > right_height)
+		return (left_height + 1);
+
+	return (right_height + 1);
 }
 
 /**
- * print_level - prints nodes at level.
+ * print_level - Prints nodes at a specific level
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer to the function to call for each node
+ * @level: Level to print
  */
-void print_level(const binary_tree_t *tree, void (*func)(int), int level)
+void print_level(const binary_tree_t *tree, void (*func)(int), size_t level)
 {
-	if (!tree)
+	if (tree == NULL)
 		return;
 
 	if (level == 1)
@@ -34,17 +43,19 @@ void print_level(const binary_tree_t *tree, void (*func)(int), int level)
 }
 
 /**
- * binary_tree_levelorder - level order traversal.
+ * binary_tree_levelorder - Goes through a binary tree level by level
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer to the function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t h, i;
+	size_t height, level;
 
-	if (!tree || !func)
+	if (tree == NULL || func == NULL)
 		return;
 
-	h = height(tree);
+	height = tree_height(tree);
 
-	for (i = 1; i <= h; i++)
-		print_level(tree, func, i);
+	for (level = 1; level <= height; level++)
+		print_level(tree, func, level);
 }
