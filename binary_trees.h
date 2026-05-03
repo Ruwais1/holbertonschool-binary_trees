d295f5c9cf8ad83759937c2dc8574fe5ca4ea026
Ruwais1 <liruwais1@gmail.com>
2026-05-03 17:33:09 +0000

    Add array_to_bst function and prototype

--- binary_trees.h
+++ binary_trees.h
@@ -51,6 +51,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* Advanced Tasks */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+bst_t *array_to_bst(int *array, size_t size);
 
 /* Tasks 20 - 25 */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
