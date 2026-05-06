127282e531618ef48b00f2862d361a2cb4019bd9
strewili <shouqaaa123@gmail.com>
2026-05-06 22:05:45 +0300

    Update 134-heap_to_sorted_array.c

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,6 +1,16 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * heap_to_sorted_array - converts heap to sorted array
+ * @heap: heap root
+ * @size: pointer to store size
+ *
+ * Return: array sorted in descending order
+ */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int *arr;
+	int *array;
 	size_t i = 0;
 
 	if (!heap || !size)
@@ -8,12 +18,12 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 
 	*size = binary_tree_size(heap);
 
-	arr = malloc(sizeof(int) * (*size));
-	if (!arr)
+	array = malloc(sizeof(int) * (*size));
+	if (!array)
 		return (NULL);
 
 	while (heap)
-		arr[i++] = heap_extract(&heap);
+		array[i++] = heap_extract(&heap);
 
-	return (arr);
+	return (array);
 }
