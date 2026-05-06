ca5ba976116016182949156f1ccff07175a9a796
strewili <shouqaaa123@gmail.com>
2026-05-06 21:52:16 +0300

    fix131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,6 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - returns size of tree
+ * @tree: root
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+			binary_tree_size(tree->right));
+}
+
+/**
+ * heap_insert - inserts value into max heap
+ * @root: root
+ * @value: value
+ *
+ * Return: new node
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new, *parent;
+	heap_t *parent, *new;
 	size_t size, i;
 	int path[64], depth = 0;
 
@@ -32,9 +55,11 @@ heap_t *heap_insert(heap_t **root, int value)
 	else
 		parent->left = new;
 
+	/* heapify up */
 	while (new->parent && new->n > new->parent->n)
 	{
 		int tmp = new->n;
+
 		new->n = new->parent->n;
 		new->parent->n = tmp;
 		new = new->parent;
