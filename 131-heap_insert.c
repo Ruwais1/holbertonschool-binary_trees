7c1c0b7721c58d0f11440350edf473b80f64b037
strewili <shouqaaa123@gmail.com>
2026-05-06 21:55:38 +0300

    Update 131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -15,32 +15,37 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
- * get_parent - finds parent for insertion
- * @root: root
+ * build_path - builds path to insertion point
+ * @path: array
+ * @depth: pointer to depth
  * @index: index
- * Return: parent node
  */
-heap_t *get_parent(heap_t *root, size_t index)
+void build_path(int *path, int *depth, size_t index)
 {
-	heap_t *parent = root;
-	size_t mask;
-
-	for (mask = 1 << (sizeof(size_t) * 8 - 2); !(index & mask); mask >>= 1)
-		;
-
-	mask >>= 1;
-
-	while (mask > 1)
+	while (index > 1)
 	{
-		parent = (index & mask) ? parent->right : parent->left;
-		mask >>= 1;
+		path[(*depth)++] = index % 2;
+		index /= 2;
 	}
-
-	return (parent);
 }
 
 /**
- * heapify_up - fixes heap after insert
+ * follow_path - moves to parent node
+ * @root: root
+ * @path: path array
+ * @depth: depth
+ * Return: parent node
+ */
+heap_t *follow_path(heap_t *root, int *path, int depth)
+{
+	while (depth > 1)
+		root = path[--depth] ? root->right : root->left;
+
+	return (root);
+}
+
+/**
+ * heapify_up - fixes heap property
  * @node: inserted node
  */
 void heapify_up(heap_t *node)
@@ -67,6 +72,7 @@ heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *parent, *new;
 	size_t size;
+	int path[64], depth = 0;
 
 	if (!root)
 		return (NULL);
@@ -76,16 +82,17 @@ heap_t *heap_insert(heap_t **root, int value)
 
 	size = binary_tree_size(*root) + 1;
 
-	parent = get_parent(*root, size);
+	build_path(path, &depth, size);
+	parent = follow_path(*root, path, depth);
 
 	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
 
-	if ((size & 1) == 0)
-		parent->left = new;
-	else
+	if (path[0])
 		parent->right = new;
+	else
+		parent->left = new;
 
 	heapify_up(new);
 
