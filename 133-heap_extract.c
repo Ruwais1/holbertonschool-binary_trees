bad6273ee99974c0aac387ae15b90c6ef23a9d1e
strewili <shouqaaa123@gmail.com>
2026-05-06 22:02:00 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,6 +1,68 @@
+#include "binary_trees.h"
+
+/**
+ * get_last - finds last node in level order
+ * @root: root
+ * Return: last node
+ */
+heap_t *get_last(heap_t *root)
+{
+	heap_t *queue[1024];
+	int front = 0, rear = 0;
+
+	queue[rear++] = root;
+
+	while (front < rear)
+	{
+		root = queue[front++];
+
+		if (root->left)
+			queue[rear++] = root->left;
+		if (root->right)
+			queue[rear++] = root->right;
+	}
+
+	return (root);
+}
+
+/**
+ * heapify_down - fixes heap after extraction
+ * @root: root
+ */
+void heapify_down(heap_t *root)
+{
+	heap_t *largest;
+	int tmp;
+
+	while (root)
+	{
+		largest = root;
+
+		if (root->left && root->left->n > largest->n)
+			largest = root->left;
+
+		if (root->right && root->right->n > largest->n)
+			largest = root->right;
+
+		if (largest == root)
+			break;
+
+		tmp = root->n;
+		root->n = largest->n;
+		largest->n = tmp;
+
+		root = largest;
+	}
+}
+
+/**
+ * heap_extract - extracts root of heap
+ * @root: root
+ * Return: value
+ */
 int heap_extract(heap_t **root)
 {
-	heap_t *last, *tmp;
+	heap_t *last;
 	int value;
 
 	if (!root || !*root)
@@ -8,7 +70,15 @@ int heap_extract(heap_t **root)
 
 	value = (*root)->n;
 
-	last = get_last_node(*root);
+	if (!(*root)->left && !(*root)->right)
+	{
+		free(*root);
+		*root = NULL;
+		return (value);
+	}
+
+	last = get_last(*root);
+
 	(*root)->n = last->n;
 
 	if (last->parent->left == last)
@@ -18,25 +88,7 @@ int heap_extract(heap_t **root)
 
 	free(last);
 
-	tmp = *root;
-	while (tmp)
-	{
-		heap_t *largest = tmp;
-
-		if (tmp->left && tmp->left->n > largest->n)
-			largest = tmp->left;
-		if (tmp->right && tmp->right->n > largest->n)
-			largest = tmp->right;
-
-		if (largest == tmp)
-			break;
-
-		int t = tmp->n;
-		tmp->n = largest->n;
-		largest->n = t;
-
-		tmp = largest;
-	}
+	heapify_down(*root);
 
 	return (value);
 }
